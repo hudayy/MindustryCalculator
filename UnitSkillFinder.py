2323fde7ac2a90335c14664d97345f0f4200d2b6
AlternateForce <43939142+AlternateForce@users.noreply.github.com>
2022-11-01 15:30:41 -0500

    Cleaned Up code, Added speed and hit size

--- UnitSkillFinder.py
+++ UnitSkillFinder.py
@@ -1,33 +1,55 @@
-#Initializes Everything
+# Initializes everything
 file = open('UnitTypes.java', 'r')
 lineList = file.readlines()
+unitReader = False
 unitList = []
+speedList = []
+hitSizeList = []
 healthList = []
 
-#Reads code line by line
+# Reads code line by line
 for i in range(len(lineList)):
     #Adds all units to unitList
     if 'new UnitType' in lineList[i]:
-        nameFound = False
+        unitReader = True
         unit = ''
-        while True:
-            foundQuote = False
-            for j in lineList[i]:
-                if foundQuote is True:
-                    if j.isalpha() is True:
-                        unit += j
-                else:
-                    if j == '\"':
-                        foundQuote = True
-                    else:
-                        continue
-            unitList.append(unit)
-            break
-
-    #Adds all units' health to healthList
-    if 'health' in lineList[i]:
-        health = ''
         for j in lineList[i]:
-            if j.isnumeric() is True:
-                health += j
-        healthList.append(int(health))
+            if j.isalpha():
+                unit += j
+            elif unit == '': continue
+            else: break
+        unitList.append(unit)
+    
+    # Collecting Information
+    if 'new Weapon' in lineList[i]:
+        unitReader = False
+    if unitReader:
+
+        #Add all units' speed to speedList
+        if 'speed' in lineList[i]:
+            speed = ''
+            for j in lineList[i]:
+                if j.isnumeric():
+                    speed += j
+            speedList.append(float(speed))
+            if len(speedList) < len(unitList): speedList.append(0.0)
+
+        # Adds all units' hitSize to hitSizeList
+        if 'hitSize' in lineList[i]:
+            hitSize = ''
+            for j in lineList[i]:
+                if j.isnumeric() or j == '.':
+                    hitSize += j
+            hitSizeList.append(float(hitSize))
+            if len(hitSizeList) < len(unitList): hitSizeList.append(0.0)
+
+        # Adds all units' health to healthList
+        if 'health' in lineList[i]:
+            health = ''
+            for j in lineList[i]:
+                if j.isnumeric():
+                    health += j
+            healthList.append(float(health))
+            if len(healthList) < len(unitList): healthList.append(0.0)
+file.flush()
+file.close()
